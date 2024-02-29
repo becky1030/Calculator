dbc25467b7b82e4469a5f0927157f3b1a70ddceb
becky1030 <pyh245@nyu.edu>
2024-02-29 16:50:44 -0500

    UI updated

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="app" class=" min-h-screen flex flex-col items-center justify-center p-6 bg-gradient-to-b from-gray-500">
-      <div class="w-full max-w-xs pb-5 px-5 shadow-blue-500 shadow-2xl rounded-xl border-4 border-gray-300">
+      <div class="w-full max-w-xs pb-5 px-5 shadow-gray-500 shadow-2xl rounded-xl border-4 border-gray-300">
         <h1 class="text-2xl font-bold text-black text-center mb-3 mt-5">Simple Calculator</h1>
         <div class="mb-4">
           <input type="number" v-model="number1" class="shadow-md shadow-gray-500 w-full px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
@@ -10,7 +10,10 @@
             <option value="add">+</option>
             <option value="subtract">-</option>
             <option value="multiply">*</option>
-            <option value="divide">/</option>
+            <option value="divide">÷</option>
+            <option value="modulus">%</option>
+            <option value="exponent">X^y</option>
+            <option value="sqrt">√</option>
           </select>
         </div>
         <div class="mb-4">
@@ -21,7 +24,7 @@
             Calculate
           </button>
         </div>
-        <p class="text-center bg-pink-300 font-bold w-full px-3 py-2 rounded-md ">Result: {{ result }}</p>
+        <p class="text-center bg-orange-400 font-bold w-full px-3 py-2 rounded-md ">Result: {{ result }}</p>
       </div>
     </div>
   </template>
