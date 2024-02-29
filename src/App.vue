afee6b35df0c25f7fb0838e74cff9f0413abf75f
becky1030 <pyh245@nyu.edu>
2024-02-29 16:00:54 -0500

    UI updates

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-    <div id="app" class=" min-h-screen flex flex-col items-center justify-center p-6 bg-black">
-      <div class="w-full max-w-xs shadow-lg ">
-        <h1 class="text-2xl font-bold text-pink-300 text-center mb-6">Simple Calculator</h1>
+    <div id="app" class=" min-h-screen flex flex-col items-center justify-center p-6 bg-gradient-to-b from-gray-500">
+      <div class="w-full max-w-xs pb-5 px-5 shadow-blue-500 shadow-2xl rounded-xl border-4 border-gray-300">
+        <h1 class="text-2xl font-bold text-black text-center mb-3 mt-5">Simple Calculator</h1>
         <div class="mb-4">
-          <input type="number" v-model="number1" class="w-full px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
+          <input type="number" v-model="number1" class="shadow-md shadow-gray-500 w-full px-3 py-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
         </div>
         <div class="mb-4">
-          <select v-model="operation" class="bg-gray-300 text-xl text-pink font-bold w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
+          <select v-model="operation" class="shadow-md shadow-gray-500 bg-gray-300 text-xl text-pink font-bold w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500">
             <option value="add">+</option>
             <option value="subtract">-</option>
             <option value="multiply">*</option>
@@ -14,10 +14,10 @@
           </select>
         </div>
         <div class="mb-4">
-          <input type="number" v-model="number2" class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
+          <input type="number" v-model="number2" class="shadow-md shadow-gray-500 w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent" />
         </div>
         <div class="mb-6">
-          <button @click="performCalculation" class="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
+          <button @click="performCalculation" class="shadow-md shadow-gray-500 w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
             Calculate
           </button>
         </div>
